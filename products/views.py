a038c364b25f3aac5c06e2c3383739ef62040d9f
tonyvives <vivestony1@gmail.com>
2021-03-22 11:48:56 -0600

    Mostrando productos

--- products/views.py
+++ products/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+
+def home(request):
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, "products/home.html", context)
